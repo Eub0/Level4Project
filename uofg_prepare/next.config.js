fab59442ba7a2d5869d3ccaac1e1a208a385b4bd
Euan McDonald <63355022+Eub0@users.noreply.github.com>
2023-12-27 14:05:55 +0000

    fixed error in next.config.js

--- uofg_prepare/next.config.js
+++ uofg_prepare/next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+
+const nextConfig = {
+	output: 'export',
+	reactStrictMode: true,
+	trailingSlash: true,
+	images: {
+		unoptimized: true,
+	},
+};
+
+module.exports = nextConfig;
