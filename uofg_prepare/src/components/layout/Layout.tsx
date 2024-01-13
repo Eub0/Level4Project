11087c7aec38b4496065214845d3bd657707a6ea
Euan McDonald <63355022+Eub0@users.noreply.github.com>
2024-01-13 15:08:34 +0000

    Template dark mode implemented

--- uofg_prepare/src/components/layout/Layout.tsx
+++ uofg_prepare/src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { HeaderContent } from '@/components/layout/HeaderContent';
 import { NavbarContent } from '@/components/layout/NavbarContent';
 import { PageContainer } from '@/components/layout/PageContainer';
-import { AppShell, Box } from '@mantine/core';
+import { AppShell, Box, useMantineColorScheme } from '@mantine/core';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { ReactNode, useState } from 'react';
@@ -15,6 +15,8 @@ export const Layout = ({ children }: LayoutProps) => {
 	const [opened, setOpened] = useState(false);
 	const router = useRouter();
 	const showLayout = !['/', '/404'].includes(router.route);
+	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  	const dark = colorScheme === 'dark';
 
 	return (
 		<>
@@ -29,14 +31,17 @@ export const Layout = ({ children }: LayoutProps) => {
                     }}
                     >
                         <AppShell.Header
-							className={classes.header}
+							className={dark ? classes.header_dark : classes.header}
 						>
 							<HeaderContent opened={opened} setOpened={setOpened} />
 						</AppShell.Header>
-						<AppShell.Navbar visibleFrom={opened ? '-xs' : 'sm'}>
+						<AppShell.Navbar 
+							visibleFrom={opened ? '-xs' : 'sm'}
+							className={dark ? classes.navbar_dark : classes.navbar}
+						>
 							<NavbarContent setNavbarOpened={setOpened} />
 						</AppShell.Navbar>
-                       <AppShell.Main>
+                       <AppShell.Main className={dark ? classes.main_dark : classes.main}>
 					        <PageContainer>{children}</PageContainer>
                         </AppShell.Main> 
 				</AppShell>
--- uofg_prepare/src/pages/_app.tsx
+++ uofg_prepare/src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import { Layout } from '@/components/layout/Layout';
 import '@/styles/globals.css'
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, useMantineColorScheme } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps}/>
       </Layout>
     </MantineProvider>
   );
