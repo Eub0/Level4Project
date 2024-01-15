4b9531ae25dc7af48fe248a77979ef5138814905
Euan McDonald <63355022+Eub0@users.noreply.github.com>
2024-01-15 17:45:26 +0000

    Created physics section, added routing, templates

---
+++ uofg_prepare/src/pages/academics/physics/index.tsx
@@ -0,0 +1,195 @@
+import { PageHeader } from "@/components/layout/PageHeader";
+import { SubSectionCard } from "@/components/shared/SubSectionCard";
+import { Stack, Title, Text, Grid, Tabs, rem } from "@mantine/core";
+import { TbWaveSine } from "react-icons/tb";
+import { SiAtom } from "react-icons/si";
+import classes from '@/styles/Tabs.module.css';
+
+export const Physics = () => {
+    const iconStyle = { width: rem(12), height: rem(12) };
+
+    return (
+      <>
+        <PageHeader
+            heading="School of Physics"
+        />
+            <Stack>
+            <Title order={1} size="h1">
+                School of Physics
+            </Title>
+            <Text>
+                Select from level 1 (Physics 1) and level 2 (Physics 2) courses, and discover what modules are covered and how you can prepare for them.
+            </Text>
+            </Stack>
+
+            <Tabs variant="unstyled" defaultValue="Physics 1" classNames={classes} pt={15}>
+                <Tabs.List grow>
+                    <Tabs.Tab value="Physics 1" leftSection={<SiAtom style={iconStyle} />}>
+                    Physics 1
+                    </Tabs.Tab>
+                    <Tabs.Tab value="Physics 2" leftSection={<TbWaveSine style={iconStyle} />}>
+                    Physics 2
+                    </Tabs.Tab>
+                </Tabs.List>
+
+                <Tabs.Panel value="Physics 1">
+                <Stack>
+                    <Text pt={10}>
+                        Physics 1 is the level 1 (first year) course for Physics. 
+                        It runs over the full accadmic year (Semesters 1 & 2), where you will cover a wide range of essesntial modules to help you going forward in physics.
+                        Click on the module pannles bellow to find more about what is covered in each, so that you can prepare yourself for begining.
+                    </Text>  
+                <Title order={3} size="h3">
+                    Modules
+                </Title>  
+                <Grid>
+                    <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                        <SubSectionCard
+                            cardTitle="Optics, Waves, & Lasers"
+                            cardText="One of the two student unions on campus."
+                            imgSrc="/images/guu.jpeg"
+                            imgAlt="UofG GUU"
+                            link="/academics/physics/physics1/opticswavesandlasers"
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                    <SubSectionCard
+                            cardTitle="Dynamics & Relativity"
+                            cardText="One of the two student unions on campus."
+                            imgSrc="/images/qmu.png"
+                            imgAlt="UofG QMU"
+                            link="/academics/physics/physics1/dynamicsandrelativity"
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                        <SubSectionCard
+                            cardTitle="Thermal Physics"
+                            cardText="One of the two student unions on campus."
+                            imgSrc="/images/guu.jpeg"
+                            imgAlt="UofG GUU"
+                            link="/academics/physics/physics1/thermalphysics"
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                        <SubSectionCard
+                            cardTitle="Electricity, Electronics & Magnetism "
+                            cardText="One of the two student unions on campus."
+                            imgSrc="/images/guu.jpeg"
+                            imgAlt="UofG GUU"
+                            link="/academics/physics/physics1/electricityelectronicsandmagnetism"
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                        <SubSectionCard
+                            cardTitle="Quantum Phenomena"
+                            cardText="One of the two student unions on campus."
+                            imgSrc="/images/guu.jpeg"
+                            imgAlt="UofG GUU"
+                            link="/academics/physics/physics1/quantumphenomena"
+                        />
+                    </Grid.Col>
+                </Grid>
+                </Stack>
+                </Tabs.Panel>
+
+                <Tabs.Panel value="Physics 2">
+                        <Stack pt={10}>
+                        <Text>
+                        Physics 2 is the level 2 (second year) courses for Physics. 
+                        It runs over the full accadmic year (Semesters 1 & 2), where you will cover a wide range of essesntial modules to help you going forward in physics.
+                        Click on the module pannles bellow to find more about what is covered in each, so that you can prepare yourself for begining.
+                        </Text>
+                        <Title order={3} size="h3">
+                            Modules
+                        </Title>
+                        <Grid>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Mathematical Techniques"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/mathematicaltechniques"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Oscillating Systems"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/oscillatingsystems"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Thermal Physics"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/thermalphysics"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Newtonian Dynamics"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/newtoniandynamics"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Electricity & Magnetism"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/electricityandmagnetism"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Physics of Solids"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/physicsofsolids"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Nuclear & Particle Physics"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/nuclearandparticlephysics"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Optics"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/optics"
+                                />
+                            </Grid.Col>
+                            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+                                <SubSectionCard
+                                    cardTitle="Classical & Quantum Waves"
+                                    cardText="One of the two student unions on campus."
+                                    imgSrc="/images/guu.jpeg"
+                                    imgAlt="UofG GUU"
+                                    link="/academics/physics/physics2/classicalandquantumwaves"
+                                />
+                            </Grid.Col>
+                        </Grid>
+                    </Stack>
+                </Tabs.Panel>
+            </Tabs>
+        </>
+    );
+}
+
+export default Physics;
---
+++ uofg_prepare/src/pages/academics/physics/physics2/nuclearandparticlephysics.tsx
@@ -0,0 +1,38 @@
+import ContentGrid from "@/components/layout/ContentGrid";
+
+export const NuclearAndParticlePhysics = () => {
+    return (
+        <>
+            <ContentGrid
+                heading="Aqueous Equilibria and pH"
+                title="Aqueous Equilibria and pH"
+                moduleAims="Aqueous Equilibria and pH aims to introduce topics relating to aqueous solutions and the factors that influence pH."
+                moduleOutline={["Water as a solvent",
+                "Hydrogen bonding",
+                "Dipoles; dielectric constant",
+                "Ice structure",
+                "Concentrations",
+                "Electrolytes",
+                "Degree of dissociation in relation to a range of solutes",
+                "Characteristics of sparingly soluble solutes, including the common ion effect and solubility product",
+                "Dissociation of weak acids and bases and associated constants such as Ka, pKa, Kb , pKb",
+                "Various definitions of acids and bases",
+                "pH and pOH calculations for strong and weak acids and bases",
+                "Degree of ionisation of weak acids and bases at various pHs",
+                "Salt hydrolysis and its influence upon pH; buffers",
+                "Le Chatelier’s Principle",
+                "Henderson-Hasselbalch equation, examples of buffers and their importance in biological systems; titrations",
+                "Indicators"]}
+                usefulWebsites=""
+                recommendedReading=""
+                cardTitle="Unit Quiz"
+                cardText="Take the quiz to test your knowlegde on the topic"
+                imgSrc="asasa"
+                imgAlt="quiz image"
+                link="asdfa"
+            />
+        </>
+    );
+}
+
+export default NuclearAndParticlePhysics;
